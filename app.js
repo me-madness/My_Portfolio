e641fbb9d8197b246905d4c766343b29ff39dd6a
me_madness <p_rangel@abv.bg>
2024-09-06 21:54:04 +0200

    i make a lith mode

--- app.js
+++ app.js
@@ -32,6 +32,13 @@ function PageTransition() {
             element.classList.add('active');
         }
     })
+
+    // Toggle Theme
+    const themeBtn = document.querySelector('.theme-btn');
+    themeBtn.addEventListener('click', () => {
+        let element = document.body;
+        element.classList.toggle('light-mode')
+    })
 }
 
 PageTransition();
